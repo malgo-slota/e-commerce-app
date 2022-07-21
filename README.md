49aed1251d1292636b95de95a81de6e27e32a609
malgo-slota <malgo.slota@gmail.com>
2022-07-21 12:45:13 +0200

    header style

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # e-commerce-app
+
+### Build with
+- HTML
+- CSS
+- JS
+- React (React Dom, React Router, React icons)
+- styled components
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,64 @@
 import React from "react";
+import styled from 'styled-components';
+import {BsSearch, BsPerson, BsCart } from 'react-icons/bs';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 function Header () {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <Nav>
+            <Logo>
+                Store
+            </Logo>
+            <NavLinks>
+                <div>
+                    <button>
+                        <GiHamburgerMenu />
+                    </button>
+                </div>
+                <div>
+                    <a href='/'>
+                        <BsSearch />
+                    </a>
+                    <a href='/'>
+                        <BsCart />
+                    </a>
+                    <a href='/'>
+                        <BsPerson />
+                    </a>
+                </div>  
+            </NavLinks> 
+        </Nav>
     );
 }; 
+
+const Nav = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0.4rem;
+`;
+
+const Logo = styled.div`
+    font-family: 'Abril Fatface', cursive;
+    font-size: 2rem;
+    color: rgb(160, 191, 48);
+    letter-spacing: 0.1rem;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  align-self: flex-end;
+  button {
+      border: none;
+      background: none;
+  }
+  a {
+      color: rgb(38, 38, 38);
+  }
+  svg {
+      font-size: 1.2rem;
+      margin: 0.4rem ;
+  }
+`;
 
 export default Header;
