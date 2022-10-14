1e9d72d899fed2bc3c6480669b1b457008b01308
malgo-slota <malgo.slota@gmail.com>
2022-10-14 14:04:40 +0200

    email validation

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
+import FormInput from './FormInput';
 
 function Subscribe () {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert("Your email has been added");
+    }
+
     return (
         <Wrapper>
             <Text>
                 <h1>Join our newsletter</h1>
                 <h2>and get 5% off your first order</h2>
             </Text>
-            <form>
-                <input aria-label="join our newsletter" type="email" placeholder='Enter your email address'></input>
-                <input type="submit" value="Submit"></input>
-        </form>
+            <form onSubmit={handleSubmit}>
+                <FormInput aria-label="join our newsletter" 
+                        type="email" 
+                        placeholder='Enter your email address'
+                        errorMessage="Invalid email adress"
+                    />
+                <button aria-label="submit">Submit</button>
+            </form>
         </Wrapper>
     );
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.section`
     background: rgb(16,17,30);
     margin-top: 6rem;
     height: 20rem;
@@ -24,20 +35,14 @@ const Wrapper = styled.div`
     form {  
         display: flex;
         flex-direction: column;
-        padding: 3rem;
+        padding: 1rem 3rem;
         input[type="email"] {
             font-size: 1.4rem;
-            padding: 1rem;
-            border: none;
             outline: none;
             background: rgb(20,22,38);
             color: rgb(242, 242,242);
-            ::placeholder{
-                color: rgb(242,242,242);
-                opacity: 0.3;
-            }
         }
-        input[type="submit"] {
+        button {
             text-transform: uppercase;
             font-size: 1rem;
             padding: 1rem;
